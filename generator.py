ef7cf9607f04a35eed59885610ef13c5ee96a2cb
Couscouz <rem.couzi@gmail.com>
2025-03-18 16:20:32 +0100

    yopmail functionnality not working anymore

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import configparser
 import requests
 import random
+import string
 import time
 import os
 
@@ -45,18 +46,49 @@ def generate_mail():
     firstname = random.choice(FIRSTNAMES).lower()
     lastname = random.choice(LASTNAMES).lower()
     symbol = random.choice(SYMBOLS)
-    number = random.randint(0, 99)
-    domain = ""
-    email = firstname + symbol + lastname + str(number) + '@' + domain
+    letters = string.ascii_lowercase + string.digits
+    salt_size = random.randint(2, 6)
+    salt = ''.join(random.choice(letters) for _ in range(salt_size))
+    domain = "yopmail.com"  #to change 
+    email = firstname + symbol + lastname + salt + '@' + domain
+    return email
+
+def check_emails(email):
+    url = f"http://www.yopmail.com/fr/inbox.php?login={email}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("Erreur lors de l'accès à la boîte de réception.")
+        print(f"Code d'erreur : {response.status_code}")
+        print(f"Contenu de la réponse : {response.text}")
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    # Trouver les e-mails dans la page (les e-mails sont dans des éléments spécifiques)
+    emails = soup.select(".lm")  # Sélecteur CSS pour les lignes d'e-mails
+    
+    if not emails:
+        print("Aucun e-mail trouvé pour l'instant.")
+        return
+    
+    print(f"E-mails reçus pour {email} :")
+    for email_item in emails:
+        subject = email_item.text.strip()
+        print(f"- Sujet : {subject}")
 
 def main():
     for _ in range(EMAILS_AMOUNT):
         start_time = time.time()
         proxy = get_proxy()
-        print(f"Using proxy: {proxy} (valid={is_valid_proxy(proxy)})")
         mail = generate_mail()
-        # attendre reception d'un mail, timeout 10min
+        print(f"Using proxy: {proxy} and {mail}")
 
+        check_emails(mail)
+        print(f"Temps écoulé : {time.time() - start_time:.2f}s\n")
+        # attendre reception d'un mail, timeout 10min
         time.sleep(1)
 
 
